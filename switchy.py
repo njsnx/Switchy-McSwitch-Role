62cb03f2758093d78af0e205f6d41f647d56ed9f
Neil Stewart <neil.stewart@cloudreach.co.uk>
2018-05-09 09:48:45 +0100

    Updated Code and Configuration

--- switchy.py
+++ switchy.py
@@ -8,6 +8,10 @@ import botocore
 import errno
 import sys
 try:
+    from switchy import MFAChameleon
+except:
+    pass
+try:
     import configparser
 except Exception:
     import ConfigParser
@@ -17,6 +21,7 @@ import os
 import json
 import datetime
 import logging
+import inspect
 
 from dateutil.tz import tzutc
 
@@ -24,16 +29,17 @@ from dateutil.tz import tzutc
 class SwitchyMcSwitchRole(object):
     """Main SwitchRole."""
 
-    def __init__(self, config_file='./configuration.json'):
+    def __init__(self, config_file='configuration.json'):
         """Init Method."""
-        
+        self.current_dir = "/".join(inspect.stack()[0][1].split("/")[:-1])
         self.configuration_file = self.load_config(config_file)
         self.aws_config_parts = self.configuration_file['configuration']['aws_config']
         self.setup_argparse()
+
         self.setup_logging()
 
     def setup_logging(self):
-
+        """Setup Logging."""
         # logging.getLogger('boto').setLevel(logging.CRITICAL)
         # logging.getLogger('botocore').setLevel(logging.CRITICAL)
         level = logging.INFO
@@ -46,13 +52,14 @@ class SwitchyMcSwitchRole(object):
         handler.setFormatter(logging.Formatter("%(message)s"))
         handler.setLevel(logging.INFO)
 
-        formatter = logging.Formatter("[%(asctime)s] %(module)s::%(funcName)s::%(lineno)d %(message)s")
-        
-        handlerDebug = logging.StreamHandler(sys.stdout)
-        handlerDebug.setFormatter(formatter)
-        handlerDebug.setLevel(logging.DEBUG)
-        self.log.addHandler(handlerDebug)
-        # "[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s','%m-%d %H:%M:%S"
+        formatter = logging.Formatter(
+            "[%(asctime)s] %(module)s::%(funcName)s::%(lineno)d %(message)s"
+        )
+
+        handler_debug = logging.StreamHandler(sys.stdout)
+        handler_debug.setFormatter(formatter)
+        handler_debug.setLevel(logging.DEBUG)
+        self.log.addHandler(handler_debug)
 
     def setup_argparse(self):
         """Setup ArgParse."""
@@ -83,7 +90,10 @@ class SwitchyMcSwitchRole(object):
     def load_config(self, config):
         """Load Config File."""
         parsed = None
-        with open(config) as f:
+        if self.current_dir == "":
+            self.current_dir = "."
+
+        with open(self.current_dir + "/" + config) as f:
             parsed = json.load(f)
         return parsed
 
@@ -101,7 +111,14 @@ class SwitchyMcSwitchRole(object):
             if self.passed_arguments.mfa:
                 mfa = self.passed_arguments.mfa
             else:
-                mfa = input("What is your MFA code for {}? ".format(self.passed_arguments.profile))
+                try:
+                    mfa = MFAChameleon(account=self.passed_arguments.profile).main()
+                except:
+                    mfa = input(
+                        "What is your MFA code for {}? ".format(
+                            self.passed_arguments.profile
+                        )
+                    )
 
             session_token = sts.get_session_token(
                 DurationSeconds=10800,
@@ -135,141 +152,168 @@ class SwitchyMcSwitchRole(object):
 
     def main(self):
         """Main CLI process."""
-        self.get_config_files()
-        if not self.passed_arguments.list:
-            self.aws_config = self.update_aws_config(
-                "default",
-                self.aws_config,
-                "profile " + self.passed_arguments.name,
-                self.aws_config
-            )
-
-            role_got = False
-            s = boto3.session.Session(
-                profile_name=self.passed_arguments.profile
-            )
-            try:
-                account = s.client('sts').get_caller_identity()['Account']
-            except Exception as e:
-                account = str(e).split('::')[2].split(":")[0]
-
-            serial = "arn:aws:iam::{}:mfa/{}".format(account, self.passed_arguments.username)
-            self.aws_config = self.update_aws_config(
-                "config",
-                self.aws_config,
-                "profile " + self.passed_arguments.name,
-                ""
-            )
-
-            sts = None
-            role_token = None
-            to_check = 'switch-role ' + self.passed_arguments.profile
-
-            if self.aws_credentials.has_section(to_check):
-
-                if self.aws_credentials.has_option(to_check, 'aws_session_token'):
-                    try:
-                        self.sts = s.client(
-                            'sts',
-                            aws_access_key_id=self.aws_credentials.get(
-                                to_check, 'aws_access_key_id'
-                            ),
-                            aws_secret_access_key=self.aws_credentials.get(
-                                to_check, 'aws_secret_access_key'
-                            ),
-                            aws_session_token=self.aws_credentials.get(
-                                to_check, 'aws_session_token'
-                            )
-                        )
-                        role_token = self.get_role(profile=self.passed_arguments)
-                        if role_token:
-                            role_got = True
-                            self.log.debug("Role retrieved")
-                    except Exception as e:
-                        self.log.error(e)
-                        pass
-
-            self.write_aws_config(self.aws_config, config_file=self.paths['config'])
-            if not role_got:
-                sts = s.client('sts')
-                session_token = self.get_new_session(sts, serial)
-
-                self.sts = s.client(
-                    'sts',
-                    aws_access_key_id=session_token['Credentials']['AccessKeyId'],
-                    aws_secret_access_key=session_token['Credentials']['SecretAccessKey'],
-                    aws_session_token=session_token['Credentials']['SessionToken']
+        try:
+            self.get_config_files()
+            if not self.passed_arguments.list:
+                self.aws_config = self.update_aws_config(
+                    "default",
+                    self.aws_config,
+                    "profile " + self.passed_arguments.name,
+                    self.aws_config
                 )
 
-                self.write_aws_config(self.aws_config, config_file=self.paths['config'])
-                # if self.passed_arguments.account is set, use that for arn, if not get from config,
-                # if not in config, ask for it and update the config
+                role_got = False
+                s = boto3.session.Session(
+                    profile_name=self.passed_arguments.profile
+                )
+                try:
+                    account = s.client('sts').get_caller_identity()['Account']
+                except Exception as e:
+                    account = str(e).split('::')[2].split(":")[0]
+
+                serial = "arn:aws:iam::{}:mfa/{}".format(
+                    account, self.passed_arguments.username
+                )
+                self.aws_config = self.update_aws_config(
+                    "config",
+                    self.aws_config,
+                    "profile " + self.passed_arguments.name,
+                    ""
+                )
+
+                sts = None
+                role_token = None
+                to_check = 'switch-role ' + self.passed_arguments.profile
+
+                if self.aws_credentials.has_section(to_check):
+
+                    if self.aws_credentials.has_option(
+                        to_check, 'aws_session_token'
+                    ):
+                        try:
+                            self.sts = s.client(
+                                'sts',
+                                aws_access_key_id=self.aws_credentials.get(
+                                    to_check, 'aws_access_key_id'
+                                ),
+                                aws_secret_access_key=self.aws_credentials.get(
+                                    to_check, 'aws_secret_access_key'
+                                ),
+                                aws_session_token=self.aws_credentials.get(
+                                    to_check, 'aws_session_token'
+                                )
+                            )
+                            role_token = self.get_role(
+                                profile=self.passed_arguments
+                            )
+                            if role_token:
+                                role_got = True
+                                self.log.debug("Role retrieved")
+                        except Exception as e:
+                            self.log.error(e)
+                            pass
+
+                self.write_aws_config(
+                    self.aws_config, config_file=self.paths['config']
+                )
+                if not role_got:
+                    sts = s.client('sts')
+                    session_token = self.get_new_session(sts, serial)
+
+                    self.sts = s.client(
+                        'sts',
+                        aws_access_key_id=session_token['Credentials']['AccessKeyId'],
+                        aws_secret_access_key=session_token['Credentials']['SecretAccessKey'],
+                        aws_session_token=session_token['Credentials']['SessionToken']
+                    )
+
+                    self.write_aws_config(
+                        self.aws_config, config_file=self.paths['config']
+                    )
+
+                    self.aws_credentials = self.update_aws_config(
+                        "credentials",
+                        self.aws_credentials,
+                        "switch-role " + self.passed_arguments.profile,
+                        session_token
+                    )
+
+                role_token = self.get_role(profile=self.passed_arguments)
 
                 self.aws_credentials = self.update_aws_config(
                     "credentials",
                     self.aws_credentials,
-                    "switch-role " + self.passed_arguments.profile,
-                    session_token
+                    self.passed_arguments.name,
+                    role_token
                 )
 
-            role_token = self.get_role(profile=self.passed_arguments)
+                # load arguments
+                # see whats in use already
+                self.write_aws_config(
+                    self.aws_credentials,
+                    config_file=self.paths['creds']
+                )
 
-            self.aws_credentials = self.update_aws_config(
-                "credentials",
-                self.aws_credentials,
-                self.passed_arguments.name,
-                role_token
-            )
+                self.log.info(
+                    """Switch role completed - You can now use --profile {} \
+in your aws cli commands or in AWS API's""".format(
+                        self.passed_arguments.name
+                    )
+                )
 
-            # load arguments
-            # see whats in use already
-            self.write_aws_config(
-                self.aws_credentials,
-                config_file=self.paths['creds']
-            )
-
-            self.log.info("""Switch role completed - You can now use --profile {} in your aws cli commands or in AWS API's""".format(self.passed_arguments.name))
-        else:
-            if self.passed_arguments.all:
-                self.log.info("Listing All Profiles")
             else:
-                self.log.info("Listing Valid Profiles")
+                if self.passed_arguments.all:
+                    self.log.info("Listing All Profiles")
+                else:
+                    self.log.info("Listing Valid Profiles")
 
-            expired = []
-            valid = []
-            for section in self.aws_config.sections():
-                if "profile" in section:
-                    if self.aws_credentials.has_section(section[8:]):
-                        if self.aws_credentials.has_option(section[8:], 'token_expiration'):
-                            expire = datetime.datetime.strptime(self.aws_credentials.get(
+                expired = []
+                valid = []
+                for section in self.aws_config.sections():
+                    if "profile" in section:
+                        if self.aws_credentials.has_section(section[8:]):
+                            if self.aws_credentials.has_option(
                                 section[8:], 'token_expiration'
-                            ), '%Y-%m-%d %H:%M:%S+00:00') + datetime.timedelta(hours=1)
-                            now = datetime.datetime.now()
-                            if now > expire:
-                                msg = "\033[91mExpired\033[00m"
-                                expired.append("{} | {}".format(msg, section[8:]))
-                            else:
-                                diff = int((expire - now).seconds / 60)
-                                if diff < 15:
-                                    msg = "\033[33m{} minutes remaining\
-                                     \033[00m".format(diff)
+                            ):
+                                expire = datetime.datetime.strptime(
+                                    self.aws_credentials.get(
+                                        section[8:], 'token_expiration'
+                                    ),
+                                    '%Y-%m-%d %H:%M:%S+00:00') + datetime.timedelta(
+                                    hours=1
+                                )
+                                now = datetime.datetime.now()
+                                if now > expire:
+                                    msg = "\033[91mExpired\033[00m"
+                                    expired.append(
+                                        "{} | {}".format(msg, section[8:])
+                                    )
                                 else:
-                                    msg = "\033[92m{} minutes remaining \
-                                    \033[00m".format(diff)
+                                    diff = int((expire - now).seconds / 60)
+                                    if diff < 15:
+                                        msg = "\033[33m{} minutes remaining\
+                                         \033[00m".format(diff)
+                                    else:
+                                        msg = "\033[92m{} minutes remaining \
+                                        \033[00m".format(diff)
 
-                                valid.append("{} | {}".format(msg, section[8:]))
+                                    valid.append(
+                                        "{} | {}".format(msg, section[8:])
+                                    )
 
-            if self.passed_arguments.all:
-                for n in expired:
-                    self.log.info(n)
+                if self.passed_arguments.all:
+                    for n in expired:
+                        self.log.info(n)
 
-            for i in valid:
-                self.log.info(i)
+                for i in valid:
+                    self.log.info(i)
+        except Exception as e:
+            self.log.info(e)
 
     def write_aws_config(self, aws_config, config_file):
         """Write updates to the Config object in the specified file."""
         self.log.debug("Writing file " + config_file)
-        
+
         config_file_path = os.path.expanduser(config_file)
         with open(config_file_path, 'w+') as fh:
             aws_config.write(fh)
@@ -283,7 +327,7 @@ class SwitchyMcSwitchRole(object):
         Returns:
             boto3.sts.assumed_role: Used later
         """
-        print(profile)
+        self.log.debug(profile)
         arg_list = {
             "RoleArn": 'arn:aws:iam::{}:role/{}'.format(
                 self.passed_arguments.account,
@@ -327,8 +371,7 @@ class SwitchyMcSwitchRole(object):
             default_region='eu-west-1'):
         """Update Config object with passed in details."""
         # Check if part to update is config or creds
-        with open('./configuration.json') as f:
-            config_parts = json.load(f)['configuration']['aws_config']
+
         self.log.debug(part)
         self.log.debug(profile)
         argument_dict = vars(self.passed_arguments)
@@ -341,7 +384,7 @@ class SwitchyMcSwitchRole(object):
             if not configuration.has_section("switch-role"):
                 configuration.add_section("switch-role")
 
-            for config_part, config in config_parts['default'].items():
+            for config_part, config in self.aws_config_parts['default'].items():
                 # if argument not provided but in config
                 argument = argument_dict[config['arg']]
 
@@ -375,12 +418,12 @@ class SwitchyMcSwitchRole(object):
             configuration.set(profile, 'output', "json")
             configuration.set(profile, 'region', "eu-west-1")
 
-            for config_part, config in config_parts['config'].items():
+            for config_part, config in self.aws_config_parts['config'].items():
                 argument = argument_dict[config['arg']]
                 self.log.debug(configuration.options(profile))
                 self.log.debug(argument)
                 if not argument and configuration.has_option(profile, config_part):
-                    self.passed_arguments.role = configuration.get(profile, config_part)
+                    argument_dict[config['arg']] = configuration.get(profile, config_part)
                 elif argument and configuration.has_option(profile, config_part):
                     configuration.set(profile, config_part, argument)
                 elif argument and not configuration.has_option(profile, config_part):
@@ -396,13 +439,7 @@ class SwitchyMcSwitchRole(object):
                         configuration.set(profile, config_part, new_value)
 
         elif part == "credentials":
-            cred_parts = {
-                'aws_access_key_id': 'AccessKeyId',
-                'aws_secret_access_key': 'SecretAccessKey',
-                'aws_security_token': 'SessionToken',
-                'aws_session_token': 'SessionToken',
-                'token_expiration': 'Expiration',
-            }
+            cred_parts = self.configuration_file['configuration']['credential_parts']
 
             if not configuration.has_section(profile):
                 configuration.add_section(profile)
@@ -410,27 +447,17 @@ class SwitchyMcSwitchRole(object):
             self.log.debug(config_update)
             for part, value in cred_parts.items():
 
-                
                 if config_update:
 
                     if value == 'Expiration':
                         value = config_update['Credentials'][value].strftime("%Y-%m-%d %H:%M:%S+00:00")
-                        configuration.set(
-                            profile,
-                            part,
-                            value
-                        )
+                        configuration.set(profile, part, value)
                     else:
-                        configuration.set(
-                            profile,
-                            part,
-                            config_update['Credentials'][value]
-                        )
+                        configuration.set(profile, part, config_update['Credentials'][value])
         try:
             self.log.debug(configuration.options(profile))
         except:
             pass
 
         return configuration
-
 SwitchyMcSwitchRole().main()
---
+++ switchy/__init__.py
@@ -0,0 +1,165 @@
+#!/Users/Neil/.pyenv/shims/python
+"""MFA Chameleon."""
+import boto3
+import base64
+import os
+import qrcode
+import argparse
+from subprocess import check_output
+try:
+    import configparser
+except:
+    import ConfigParser
+import clipboard
+
+
+class MFAChameleon:
+    """MFA Chameleon Class."""
+
+    def __init__(self, account=None):
+        """Init Method."""
+        self.debug = False
+        self.new = False
+        self.serial = None
+
+        if not account:
+            print("Running here")
+            parser = argparse.ArgumentParser()
+            parser.add_argument(
+                "account", type=str, help="The account to work with"
+            )
+            parser.add_argument("-n", "--new", action='store_true')
+            parser.add_argument("-s", "--serial")
+            parser.add_argument("-d", "--debug", action='store_true')
+            parser.add_argument("-l", "--list", action='store_true')
+
+            self.args = parser.parse_args()
+            self.account = self.args.account
+            self.debug = self.args.debug
+            self.new = self.args.new
+            self.serial = self.args.serial
+        if account:
+            self.account = account
+
+        self.session = boto3.session.Session(
+            profile_name=os.environ['mfa_cham_profile'],
+            region_name="eu-west-1"
+        )
+        self.location = "{}/.mfa".format(os.path.expanduser('~'))
+
+    def encrypt(self, content, account):
+        """Encrypt a value."""
+        return base64.b64encode(
+            self.session.client('kms').encrypt(
+                KeyId=os.environ['mfa_cham_key'],
+                Plaintext=content,
+                EncryptionContext={
+                    "MFAChameleon": "Encrypt{}".format(account.title())
+                }
+            )['CiphertextBlob']
+        ).decode("utf-8")
+
+    def decrypt(self, blob, account):
+        """Decrypt a value."""
+        serial = self.session.client('kms').decrypt(
+            CiphertextBlob=base64.b64decode(blob),
+            EncryptionContext={
+                "MFAChameleon": "Encrypt{}".format(account.title())
+            }
+        )['Plaintext'].decode('utf-8')
+        if self.debug:
+            print("DEBUG: Serial: {}".format(serial))
+        return serial
+
+    def get_mfa(self, account):
+        """Get MFA."""
+        config, cfg = self.load_config()
+
+        if config.has_section(account) and config.has_option(
+            account, "serial"
+        ):
+            serial = config.get(account, "serial")
+            serial = self.decrypt(serial, account)
+            output = check_output(
+                ["/usr/local/bin/oathtool", "--totp", "-b", serial]
+            ).decode('utf-8')
+            clipboard.copy(output)
+
+            return output.strip('\n'), serial
+
+    def load_config(self):
+        """Load Config."""
+        cfgfile = "{}/accounts".format(self.location)
+
+        try:
+            config = configparser.ConfigParser()
+        except:
+            config = ConfigParser.ConfigParser()
+
+        config.read([cfgfile])
+
+        return config, self.location
+
+    def save(self, content, account):
+        """Save the config file."""
+        config, location = self.load_config()
+
+        if config.has_section(account):
+            print("Section already exists")
+        else:
+            config.add_section(account)
+            config.set(account, "serial", content)
+
+            with open("{}/accounts".format(location), 'w+') as fh:
+                config.write(fh)
+
+    def create_qr(self, serial, account):
+        """Create QR Code."""
+        x = "otpauth://totp/{account}?secret={serial}&issuer={account}".format(
+            account=account,
+            serial=serial
+        )
+
+        qr = qrcode.QRCode(
+            version=1,
+            box_size=2
+        )
+
+        qr.add_data(x)
+
+        img = qr.make_image()
+        img.save(
+            "{}/{}.png".format(
+                self.location,
+                account
+            ),
+            "PNG"
+        )
+
+        check_output(["open", "{}/{}.png".format(
+            self.location,
+            account
+        )]).decode('utf-8')
+
+    def main(self):
+        """Main method."""
+        account = self.account
+        is_new = self.new
+        serial = self.serial
+
+        if is_new and serial:
+            print("Creating new setup for {}".format(account))
+
+            encrypted = self.encrypt(serial, account)
+            self.save(encrypted, account)
+            mfa, serial = self.get_mfa(account)
+            self.create_qr(serial, account)
+
+        else:
+            mfa, serial = self.get_mfa(account)
+        return mfa
+
+if __name__ == '__main__':
+
+    mfa = MFAChameleon()
+    print(mfa.main())
